cd9c7766503dafc86fd2d25bff060065aee71510
Siddhant Jaiswal <34509466+siddhant-dev@users.noreply.github.com>
2019-11-09 15:28:16 +0530

    remove api keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  config: {
-    apiKey: 'AIzaSyALcNotoOSzya4awj_1iHMXh71rbeonRn8',
-    authDomain: 'cloudfirestorelearning.firebaseapp.com',
-    databaseURL: 'https://cloudfirestorelearning.firebaseio.com',
-    projectId: 'cloudfirestorelearning',
-    storageBucket: 'cloudfirestorelearning.appspot.com',
-    messagingSenderId: '681859861309',
-    appId: '1:681859861309:web:1726f8a46bf9b5b48a9354'
-  },
 };
 
 /*
